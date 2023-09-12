8c5d9f6048a02c040cf8bfb2916cd2cbfa952362
Jon McLean <jon@mclean.one>
2023-09-13 08:34:32 +1000

    Added initial websocket interface

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-from hand_segment import HandSegmenter
+# from hand_segment import HandSegmenter
 
-segmenter = HandSegmenter(maxHands=1, boundingBoxOffset=(20, 20))
+import base64
+import os
+import cv2 as cv
+import numpy as np
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
+
+# segmenter = HandSegmenter(maxHands=1, boundingBoxOffset=(20, 20))
+
+app = Flask(__name__)
+app.config["SECRET"] = "obivously not a secret"
+
+socketio = SocketIO(app)
+
+savePath = os.path.join(os.getcwd(), "img.jpg")
+
+def convert_encoded_image(encoded):
+    data = encoded.split(',')[1]
+    bytes = base64.b64decode(data)
+    array = np.frombuffer(bytes, dtype=np.uint8)
+    image = cv.imdecode(array, cv.IMREAD_COLOR)
+    return image
+
+@socketio.on("connect")
+def connect():
+    print("Connected")
+    emit("response", {"data": "connected"})
+
+@socketio.on("image")
+def received_image(encoded_image):
+    image = convert_encoded_image(encoded_image)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    cv.imwrite(savePath, gray)
+    emit("response", {"data": "image received"})
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=8765, host='0.0.0.0')
